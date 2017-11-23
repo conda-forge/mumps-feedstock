3c1a8e6bc78d0b33fae65cf4d2fe07ed5bb3af87
Richard Otis <rotis@jpl.nasa.gov>
2017-11-23 12:24:03 -0800

    WIP: Path fixes

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,15 +1,12 @@
 setlocal EnableDelayedExpansion
 
-xcopy ..\CMakeLists.txt .
-
-:: Make a build folder and change to it.
+cd work
 mkdir build
 cd build
 
 :: Configure using the CMakeFiles
 cmake -G "MinGW Makefiles" ^
-      -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
-      -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
+      -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%\mingw-w64" ^
       -DCMAKE_BUILD_TYPE:STRING=Release ^
       ..
 if errorlevel 1 exit 1
