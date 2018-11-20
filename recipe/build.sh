e8322dfeaf2eac92a8cb879f4134966bb891c6a8
Min RK <benjaminrk@gmail.com>
2018-11-20 10:23:50 +0100

    restore mumps_mpi.h header
    got lost in the 5.1.2 upgrade
    and apparently is used in some packages like ipopt

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,7 +6,8 @@ make all
 
 cp lib/*.a ${PREFIX}/lib
 cp libseq/*.a ${PREFIX}/lib
-cp libseq/*.h ${PREFIX}/include
+cp libseq/mpi.h ${PREFIX}/include/mumps_mpi.h
+cp libseq/mpif.h ${PREFIX}/include/mumps_mpif.h
 cp include/*.h ${PREFIX}/include
 
 cd examples
