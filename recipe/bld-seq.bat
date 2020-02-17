408b983154ba55181dc8604eafb9107edb57c917
Richard Otis <rotis@jpl.nasa.gov>
2020-02-17 09:35:01 -0800

    FIX: bld-seq.bat: Force use of clang-cl on Windows

--- recipe/bld-seq.bat
+++ recipe/bld-seq.bat
@@ -13,6 +13,7 @@ cmake -G "NMake Makefiles" ^
       -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
       -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
       -DCMAKE_VERBOSE_MAKEFILE=1 ^
+      -DCMAKE_C_COMPILER=clang-cl ^
       -DCMAKE_BUILD_TYPE:STRING=Release ^
       ..
 if errorlevel 1 exit 1
@@ -20,11 +21,11 @@ cmake --build . --config Release --target install
 if errorlevel 1 exit 1
 %src%\build\c_example < %src%\examples\input_simpletest_real
 if errorlevel 1 exit 1
-rem %src%\build\ssimpletest < %src%\examples\input_simpletest_real
+%src%\build\ssimpletest < %src%\examples\input_simpletest_real
 if errorlevel 1 exit 1
-rem %src%\build\dsimpletest < %src%\examples\input_simpletest_real
+%src%\build\dsimpletest < %src%\examples\input_simpletest_real
 if errorlevel 1 exit 1
-rem %src%\build\csimpletest < %src%\examples\input_simpletest_cmplx
+%src%\build\csimpletest < %src%\examples\input_simpletest_cmplx
 if errorlevel 1 exit 1
-rem %src%\build\zsimpletest < %src%\examples\input_simpletest_cmplx
+%src%\build\zsimpletest < %src%\examples\input_simpletest_cmplx
 if errorlevel 1 exit 1
