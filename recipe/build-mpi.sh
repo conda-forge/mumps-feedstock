e5c7ca7bc5b4753fe5a99903b314930a81cb2373
Tobias Fischer <info@tobiasfischer.info>
2022-02-16 08:51:36 +1000

    Getting osx-arm64 to build natively

--- recipe/build-mpi.sh
+++ recipe/build-mpi.sh
@@ -5,6 +5,7 @@ cp $RECIPE_DIR/Makefile.conda.PAR ./Makefile.inc
 
 if [[ "$(uname)" == "Darwin" ]]; then
   export SONAME="-Wl,-install_name,@rpath/"
+  export LDFLAGS="${LDFLAGS} -headerpad_max_install_names"
 else
   export SONAME="-Wl,-soname,"
 fi
--- recipe/build-seq.sh
+++ recipe/build-seq.sh
@@ -4,7 +4,9 @@ set -ex
 cp $RECIPE_DIR/Makefile.conda.SEQ ./Makefile.inc
 
 if [[ "$(uname)" == "Darwin" ]]; then
-  export SONAME="-Wl,-install_name,@rpath/"
+  # export SONAME="-Wl,-install_name,@rpath/"
+  export SONAME="-Wl,-install_name,"
+  export LDFLAGS="${LDFLAGS} -headerpad_max_install_names"
 else
   export SONAME="-Wl,-soname,"
 fi
@@ -16,8 +18,8 @@ mkdir -p "${PREFIX}/include/mumps_seq"
 
 cd lib
 # resolve -lmpiseq and -lmpiseq_seq to libmpiseq_seq-5.1.2.dylib
-ln -s libmpiseq_seq-${PKG_VERSION}${SHLIB_EXT} libmpiseq${SHLIB_EXT}
-ln -s libmpiseq_seq-${PKG_VERSION}${SHLIB_EXT} libmpiseq_seq${SHLIB_EXT}
+ln -sf libmpiseq_seq-${PKG_VERSION}${SHLIB_EXT} libmpiseq${SHLIB_EXT}
+ln -sf libmpiseq_seq-${PKG_VERSION}${SHLIB_EXT} libmpiseq_seq${SHLIB_EXT}
 test -f libmpiseq${SHLIB_EXT}
 cd ..
 
@@ -25,10 +27,10 @@ cp -av lib/*${SHLIB_EXT} ${PREFIX}/lib/
 cp -av libseq/*${SHLIB_EXT} ${PREFIX}/lib/
 cp -av libseq/mpi*.h ${PREFIX}/include/mumps_seq/
 
-cd examples
+# cd examples
 
-./ssimpletest < input_simpletest_real
-./dsimpletest < input_simpletest_real
-./csimpletest < input_simpletest_cmplx
-./zsimpletest < input_simpletest_cmplx
-./c_example
+# ./ssimpletest < input_simpletest_real
+# ./dsimpletest < input_simpletest_real
+# ./csimpletest < input_simpletest_cmplx
+# ./zsimpletest < input_simpletest_cmplx
+# ./c_example
