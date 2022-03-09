184b83e2268a26f1915761b47936b9a3d584234b
Tobias Fischer <info@tobiasfischer.info>
2022-03-10 09:20:15 +1000

    Update build-seq.sh

--- recipe/build-seq.sh
+++ recipe/build-seq.sh
@@ -3,6 +3,15 @@ set -ex
 
 cp $RECIPE_DIR/Makefile.conda.SEQ ./Makefile.inc
 
+
+if [[ "$target_platform" == linux-* || "$target_platform" == "osx-arm64"  ]]
+then
+  # Workaround for https://github.com/conda-forge/scalapack-feedstock/pull/30#issuecomment-1061196317
+  # As of March 2022, on macOS arm64 gfortran 9 is still used
+  export FFLAGS="${FFLAGS} -fallow-argument-mismatch"
+  export OMPI_FCFLAGS=${FFLAGS}
+fi
+
 if [[ "$(uname)" == "Darwin" ]]; then
   # export SONAME="-Wl,-install_name,@rpath/"
   export SONAME="-Wl,-install_name,"
