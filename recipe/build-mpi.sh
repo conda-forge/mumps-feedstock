6f5203e0f7aa93d705df942ba59aa82f15a97684
Tobias Fischer <info@tobiasfischer.info>
2022-03-10 11:14:42 +1000

    Update build-mpi.sh

--- recipe/build-mpi.sh
+++ recipe/build-mpi.sh
@@ -9,7 +9,16 @@ if [[ "$target_platform" == linux-* || "$target_platform" == "osx-arm64"  ]]
    # As of March 2022, on macOS (Intel) gfortran 9 is still used
    export FFLAGS="${FFLAGS} -fallow-argument-mismatch"
    export OMPI_FCFLAGS=${FFLAGS}
- fi
+fi
+ 
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
+  # This is only used by open-mpi's mpicc
+  # ignored in other cases
+  export OMPI_CC=$CC
+  export OMPI_CXX=$CXX
+  export OMPI_FC=$FC
+  export OPAL_PREFIX=$PREFIX
+fi
 
 if [[ "$(uname)" == "Darwin" ]]; then
   export SONAME="-Wl,-install_name,@rpath/"
