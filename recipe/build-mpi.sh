f4a33bcdd59e6834d2c9bd30efbdf6c4af6ee1ef
Tobias Fischer <info@tobiasfischer.info>
2022-03-10 10:12:47 +1000

    Update build-mpi.sh

--- recipe/build-mpi.sh
+++ recipe/build-mpi.sh
@@ -3,6 +3,14 @@ set -ex
 
 cp $RECIPE_DIR/Makefile.conda.PAR ./Makefile.inc
 
+if [[ "$target_platform" == linux-* || "$target_platform" == "osx-arm64"  ]]
+ then
+   # Workaround for https://github.com/conda-forge/scalapack-feedstock/pull/30#issuecomment-1061196317
+   # As of March 2022, on macOS (Intel) gfortran 9 is still used
+   export FFLAGS="${FFLAGS} -fallow-argument-mismatch"
+   export OMPI_FCFLAGS=${FFLAGS}
+ fi
+
 if [[ "$(uname)" == "Darwin" ]]; then
   export SONAME="-Wl,-install_name,@rpath/"
   export LDFLAGS="${LDFLAGS} -headerpad_max_install_names"
