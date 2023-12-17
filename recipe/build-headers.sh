79f6dfa57c19eb501ca17a41cb4c31667922b6e0
Silvio Traversaro <silvio@traversaro.it>
2023-12-17 17:31:00 +0100

    Update build-headers.sh

--- recipe/build-headers.sh
+++ recipe/build-headers.sh
@@ -1,4 +1,10 @@
+echo "build-headers pwd"
 pwd
+echo "build-headers ls include/"
 ls include/
+echo "build-headers ls ../include/"
+ls ../include/
 mkdir -p $PREFIX/include
 cp -av include/*.h $PREFIX/include/
+# To also copy generated headers, see https://github.com/conda-forge/mumps-feedstock/issues/100
+cp -av ../include/*.h $PREFIX/include/
