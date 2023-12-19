5027e809d6b01977b8a8a2f4b27c15922ea381f4
Min RK <benjaminrk@gmail.com>
2023-12-19 13:01:19 +0100

    update OMPI env for mpiexec

--- recipe/mpiexec.sh
+++ recipe/mpiexec.sh
@@ -7,6 +7,7 @@ if [[ "$mpi" == "mpich" ]]; then
 elif [[ "$mpi" == "openmpi" ]]; then
     export OMPI_MCA_plm=isolated
     export OMPI_MCA_rmaps_base_oversubscribe=yes
+    export OMPI_MCA_btl=tcp,self
     export OMPI_MCA_btl_vader_single_copy_mechanism=none
     export OMPI_MCA_coll_basic_priority=100
     mpiexec="mpiexec --allow-run-as-root"
