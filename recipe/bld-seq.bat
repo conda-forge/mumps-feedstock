92ecc0cf56e9d6e5386c907a02b16990ce7d2e1f
Silvio Traversaro <silvio@traversaro.it>
2024-02-05 13:26:14 +0100

    Update bld-seq.bat

--- recipe/bld-seq.bat
+++ recipe/bld-seq.bat
@@ -10,8 +10,9 @@ copy %src%\src\mumps_int_def32_h.in %src%\include\mumps_int_def.h
 mkdir build
 cd build
 
-set "CC=clang-cl.exe"
-set "CXX=clang-cl.exe"
+set "CC=clang-cl"
+set "CXX=clang-cl"
+set "FC=flang-new"
 
 :: Configure using the CMakeFiles
 cmake -G "Ninja" ^
